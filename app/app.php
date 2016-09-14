0f966d2f24fcbcfdbd50baec98d3917437bd7426
HK Kahng and Katy Henning <katy@>
2016-09-14 13:57:42 -0700

    add a user friendly front end

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/allergies.php";
+    require_once __DIR__."/../src/FindAndReplace.php";
 
     $app = new Silex\Application();
     $app['debug'] = true;
@@ -11,6 +11,14 @@
         return $app['twig']->render('home.html.twig');
     });
 
+    $app->post('/far', function() use ($app) {
+        $input1 = $_POST['sentence'];
+        $input2 = $_POST['word_to_replace'];
+        $input3 = $_POST['replacement_word'];
+        $replacer = new FindAndReplace;
+        $result = $replacer->truly_impossible($input1, $input2, $input3);
+        return $app['twig']->render('result.html.twig', array('result'=>$result));
+    });
 
     return $app;
   ?>
